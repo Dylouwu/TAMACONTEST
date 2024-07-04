5486e6d322acdfcaa0c2fb9eb56a0298e5a99396
Dylouwu <diloulefilou@gmail.com>
2024-07-04 19:27:04 +0900

    Day 9 100%

--- test_9/C.py
+++ test_9/C.py
@@ -1,7 +1,11 @@
 def main():
-    N = int(input())
-    #N, M = map(int, input().split())
-
+    A, B, K = map(int, input().split())
+    count = 0
+    while(A < B):
+        A *= K
+        count += 1
+    
+    print(count)
     
 if __name__ == "__main__":
     main()
--- test_9/G.py
+++ test_9/G.py
@@ -1,6 +1,11 @@
 def main():
-    N = int(input())
-
-    
+    N, M = map(int,input().split())
+    A = list(map(int,input().split()))
+    votes = [0] * (N+1)
+    for i in range(M):
+        votes[A[i]] += 1
+        print(votes.index(max(votes)))
+        
+        
 if __name__ == "__main__":
     main()
