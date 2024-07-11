c2a890c2cdefc762c11dea84cbb9de418e590595
Dylouwu <diloulefilou@gmail.com>
2024-07-11 19:39:23 +0900

    day10 done !

--- test_A/D.py
+++ test_A/D.py
@@ -1,5 +1,16 @@
 def main():
     N = int(input())
+    P = 0
+    board = []
+    A = list(map(int, input().split()))
+    for i in range(N):
+        board.append(0)
+        board = [piece + A[i] for piece in board]
+    
+    for piece in board:
+        if piece > 3:
+            P += 1
+    print(P)
 
     
 if __name__ == "__main__":
